a726c8e35f9cdcccd2459b3a538f0dfbc2682777
Jan Vomlel <jv@2scale.cz>
2023-01-03 02:48:57 +0100

    add unit tests with not happy day

--- backend/test/shoppingList.test.js
+++ backend/test/shoppingList.test.js
@@ -57,6 +57,29 @@ describe('shopping list', () => {
     });
   });
 
+  // create shopping list wrong session id
+  describe('create shopping list wrong session id', () => {
+    it('should not create a shopping list', () => {
+      const req = {
+        body: {
+          "name": shoppingListName
+        },
+        Headers: {
+          sessionId: 'wrong session id',
+          contentType: 'application/json',
+        },
+      };
+      const res = {
+        status() {},
+        json() {},
+      };
+      shoppingListController.create(req, res).then((result) => {
+        expect(result).to.be.an('object');
+        expect(result.message).to.equal('Session id is invalid.');
+      });
+    });
+  });
+
   // add item to shopping list (update)
   describe('add item to shopping list', () => {
     it('should add item to a shopping list', () => {
@@ -86,6 +109,31 @@ describe('shopping list', () => {
     });
   });
 
+    // add item to shopping list (update) wrong shopping list id
+    describe('add item to shopping list wrong shopping list id', () => {
+      it('should not add item to a shopping list', () => {
+        const req = {
+          body: {
+            "name": "Testovaci polozka",
+            "quantity": 1,
+            "shoppingListId": 'wrong shopping list id'
+          },
+          Headers: {
+            sessionId: sessionId,
+            contentType: 'application/json',
+          },
+        };
+        const res = {
+          status() {},
+          json() {},
+        };
+        shoppingListController.addItem(req, res).then((result) => {
+          expect(result).to.be.an('object');
+          expect(result.message).to.equal('Shopping list id is invalid.');
+        });
+      });
+    });
+
   // get one shopping list
   describe('get shopping list', () => {
     it('should get a shopping list', () => {
@@ -109,6 +157,29 @@ describe('shopping list', () => {
         expect(result.items).to.be.an('array');
         expect(result.items[0].name).to.equal("Testovaci polozka");
         expect(result.items[0].quantity).to.equal(1);
+      });
+    });
+  });
+
+  // get one shopping list wrong shopping list id
+  describe('get shopping list wrong shopping list id', () => {
+    it('should not get a shopping list', () => {
+      const req = {
+        params: {
+          id: 'wrong shopping list id',
+        },
+        Headers: {
+          sessionId: sessionId,
+          contentType: 'application/json',
+        },
+      };
+      const res = {
+        status() {},
+        json() {},
+      };
+      shoppingListController.getOne(req, res).then((result) => {
+        expect(result).to.be.an('object');
+        expect(result.message).to.equal('Shopping list id is invalid.');
       });
     });
   });
@@ -137,6 +208,26 @@ describe('shopping list', () => {
     });
   });
 
+    // get all shopping lists wrong session id
+    describe('get all shopping lists wrong session id', () => {
+      it('should not get all shopping lists', () => {
+        const req = {
+          Headers: {
+            sessionId: 'wrong session id',
+            contentType: 'application/json',
+          },
+        };
+        const res = {
+          status() {},
+          json() {},
+        };
+        shoppingListController.getAll(req, res).then((result) => {
+          expect(result).to.be.an('object');
+          expect(result.message).to.equal('Session id is invalid.');
+        });
+      });
+    });
+
   // delete shopping list
   describe('delete shopping list', () => {
     it('should delete a shopping list', () => {
@@ -163,6 +254,29 @@ describe('shopping list', () => {
       });
     });
   });
+
+    // delete shopping list wrong shopping list id
+    describe('delete shopping list wrong shopping list id', () => {
+      it('should not delete a shopping list', () => {
+        const req = {
+          params: {
+            id: 'wrong shopping list id',
+          },
+          Headers: {
+            sessionId: sessionId,
+            contentType: 'application/json',
+          },
+        };
+        const res = {
+          status() {},
+          json() {},
+        };
+        shoppingListController.delete(req, res).then((result) => {
+          expect(result).to.be.an('object');
+          expect(result.message).to.equal('Shopping list id is invalid.');
+        });
+      });
+    });
 
   // get all shopping lists
   describe('get all shopping lists (should be 0)', () => {
